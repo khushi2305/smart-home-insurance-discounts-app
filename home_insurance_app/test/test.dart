24db8dcb840782ffc4613ac4ee1b98eced1bb719
khushi2305 <guptakhushi2404@gmail.com>
2020-06-02 17:28:08 +0530

    Added a widget test

--- home_insurance_app/test/test.dart
+++ home_insurance_app/test/test.dart
@@ -3,22 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:homeinsuranceapp/main.dart';
 
-/*void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+void main() {
+  testWidgets('Widget Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+    // Verify that our home page includes the introduction text .
+    expect(find.text("All your protection under one roof .Take Home Insurance now and secure your future. Don't forget to exlore the exciting discounts available "), findsOneWidget);
+    
+    // Test that icon button in the drawer is working - TODO 
+    /*final drawerOpenButton = find.byTooltip("Open navigation menu");
+    await driver.waitFor(drawerOpenButton);*/
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    
   });
 }
-*/
+
