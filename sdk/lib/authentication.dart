de66a764e3113249b153306713783713a188c275
oshhh <osheen18059@iiitd.ac.in>
2020-06-02 22:36:53 +0530

    added mock class for GoogleSignIn API

--- sdk/lib/authentication.dart
+++ sdk/lib/authentication.dart
@@ -8,16 +8,17 @@ class Authentication {
   static const int LOGIN_TIMEOUT_DURATION = 60; // User will have to enter email and password
   static const int LOGOUT_TIMEOUT_DURATION = 3;
 
-  GoogleSignIn _googleSignIn;
+  int testing;
+  GoogleSignInAPI googleSignInAPI;
 
-  Authentication() {
-    _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
+  Authentication({this.testing = 0}) {
+    googleSignInAPI = GoogleSignInAPI(testing: testing);
   }
 
   Future<String> login() async {
     // returns status "login successful", "login failed", "login time-out", "already logged in"
     try{
-      await _googleSignIn.signIn().timeout(const Duration(seconds: LOGIN_TIMEOUT_DURATION));
+      await googleSignInAPI.signIn().timeout(const Duration(seconds: LOGIN_TIMEOUT_DURATION));
       return "login successful";
     } catch (error) {
       return "login failed";
@@ -26,10 +27,35 @@ class Authentication {
   }
   Future<String> logout() async {
     // returns status "logout successful", "logout failed", "logout timed-out", "not logged in"
-    await _googleSignIn.disconnect().timeout(const Duration(seconds: LOGOUT_TIMEOUT_DURATION));
+    await googleSignInAPI.disconnect().timeout(const Duration(seconds: LOGOUT_TIMEOUT_DURATION));
 
   }
 //  User getUserDetails() {
 //    // returns an instance of class User
 //  }
+}
+
+// For testing purpose
+// Behaves exactly like GoogleSignIn object if testing = 0
+// Behaves otherwise if testing != 0 (can define test cases for them)
+class GoogleSignInAPI {
+  GoogleSignIn _googleSignIn;
+  int testing;
+  GoogleSignInAPI({this.testing = 0}) {
+    if(testing == 0) {
+      _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
+    }
+  }
+
+  Future<void> signIn() async {
+    if(testing == 0) {
+      await _googleSignIn.signIn();
+    }
+  }
+  Future<void> disconnect() async {
+    if(testing == 0) {
+      await _googleSignIn.disconnect();
+    }
+  }
+
 }
