c3bd6d2458268860eaf619f11aeec25653bcd475
anjalisoni3655 <anjalisoni3655@gmail.com>
2020-06-03 12:35:16 +0530

    added the resourcepickerURL

--- sdk/lib/device_access_authorization.dart
+++ sdk/lib/device_access_authorization.dart
@@ -4,18 +4,26 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:googleapis_auth/auth_io.dart';
 
 
-class DeviceAccessAuthorization {
+class urlMaker{
   String _accessToken;
   String _refreshToken;
   String _clientId;
   String _clientSecret;
   String _resourcePickerURL;
   List<String> _scope;
+  String EnterpriseId;
+  String PCM_url="https://nestservices.google.com/partnerconnections";
+  String redirect_URL ="urn:ietf:wg:oauth:2.0:oob";
+   String url ;
 
-  DeviceAccessAuthorization(String clientId, String clientSecret, List<String> scope) {
+
+  urlMaker(String clientId, String clientSecret, List<String> scope, String enterpriseId) {
     this._clientId = clientId;
     this._clientSecret = clientSecret;
     this._scope = scope;
+    this.enterpriseId = enterpriseId;
+    url ="$PCM_url/enterpriseId/auth?client_id=$_clientId&redirect_uri=$redirect_URL&response_type=code&scope=$_scope&state=state";
+    Uri.parse(url);
   }
 
   Future<String> askForAuthorization() async {
