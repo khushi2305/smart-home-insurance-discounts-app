1302be958aee24df10c6d5ce91a464596b8edce7
anjalisoni3655 <anjalisoni3655@gmail.com>
2020-06-03 10:55:31 +0530

    added the url and responseJson

---
+++ sdk/test/access_devices_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sdk/device_access_authorization.dart';
+
+void main() {
+  test('empty passing test', () {
+    expect((){return 1;}, 1);
+  });
+}
