639781d60e183731ba73ec9e18df098d02acbf62
oshhh <osheen18059@iiitd.ac.in>
2020-06-04 16:15:36 +0530

    formatted code

--- sdk/lib/sdk.dart
+++ sdk/lib/sdk.dart
@@ -21,48 +21,60 @@ class SDK {
   String enterpriseId;
   String redirectURL;
 
-  SDK(String clientId, String clientSecret, String enterpriseId, String redirectURL, {this.loginTimeoutDuration, this.logoutTimeoutDuration, this.isSignedInTimeoutDuration, this.resourcePickerTimeoutDuration}) {
+  SDK(String clientId, String clientSecret, String enterpriseId,
+      String redirectURL,
+      {this.loginTimeoutDuration,
+      this.logoutTimeoutDuration,
+      this.isSignedInTimeoutDuration,
+      this.resourcePickerTimeoutDuration}) {
     this.clientId = clientId;
     this.clientSecret = clientSecret;
     this.enterpriseId = enterpriseId;
     this.redirectURL = redirectURL;
 
-    _login = new Login(logoutTimeoutDuration: logoutTimeoutDuration, loginTimeoutDuration: loginTimeoutDuration, isSignedInTimeoutDuration: isSignedInTimeoutDuration);
-    _resourcePicker = new ResourcePicker(clientId, clientSecret, enterpriseId, redirectURL, resourcePickerTimeoutDuration: resourcePickerTimeoutDuration);
+    _login = new Login(
+        logoutTimeoutDuration: logoutTimeoutDuration,
+        loginTimeoutDuration: loginTimeoutDuration,
+        isSignedInTimeoutDuration: isSignedInTimeoutDuration);
+    _resourcePicker = new ResourcePicker(
+        clientId, clientSecret, enterpriseId, redirectURL,
+        resourcePickerTimeoutDuration: resourcePickerTimeoutDuration);
   }
 
-  Future<String> login() =>  _login.login();
-  Future<String> logout() =>  _login.logout();
+  Future<String> login() => _login.login();
+  Future<String> logout() => _login.logout();
   Future<User> getUserDetails() => _login.getUserDetails();
 
   Future<String> requestDeviceAccess() async {
     String status = await _resourcePicker.askForAuthorization();
-    if(status == "authentication successful") {
-      _accessDevices = new AccessDevices(_resourcePicker.accessToken, this.enterpriseId);
+    if (status == "authentication successful") {
+      _accessDevices =
+          new AccessDevices(_resourcePicker.accessToken, this.enterpriseId);
     }
     return status;
   }
 
   Future<String> getAllDevices() async {
-    if(_accessDevices == null) {
+    if (_accessDevices == null) {
       return null;
     } else {
       return await _accessDevices.getAllDevices();
     }
   }
+
   Future<String> getAllStructures() async {
-    if(_accessDevices == null) {
+    if (_accessDevices == null) {
       return null;
     } else {
       return await _accessDevices.getAllStructures();
     }
   }
+
   Future<String> getDeviceStatus(String deviceId) async {
-    if(_accessDevices == null) {
+    if (_accessDevices == null) {
       return null;
     } else {
       return await _accessDevices.getDeviceStatus(deviceId);
     }
   }
-
 }
