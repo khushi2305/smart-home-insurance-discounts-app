aa2cd3b65cda1646300082c455e66ee03ecd7820
khushi2305 <guptakhushi2404@gmail.com>
2020-06-04 18:22:40 +0530

    Changed test.dart filename to widget_test.dart.

---
+++ home_insurance_app/lib/pages/choose_policy.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class Mapping{
+  String user_choice;
+  int index;
+  Mapping(int index, String user_choice){
+    this.index=index;
+    this.user_choice=user_choice;
+
+  }
+
+}
+
+class DisplayPolicies extends StatefulWidget {
+
+  @override
+  _DisplayPoliciesState createState() => _DisplayPoliciesState();
+}
+ Map data = {}; 
+
+class _DisplayPoliciesState extends State<DisplayPolicies> {
+   @override
+  Widget build(BuildContext context) {
+  // data stores the policies available for the user as a key-value pair.
+   data =ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+        appBar:AppBar(
+          title:Text('Home Insurance Company '),
+          centerTitle: true ,
+          backgroundColor: Colors.brown,
+        ),
+        floatingActionButton:FloatingActionButton.extended(
+          onPressed:(){
+            Navigator.pop(context,'/');
+          },// Goes to the payment page 
+          backgroundColor: Colors.lightBlueAccent,  
+          icon:Icon(Icons.payment),
+          label:Text('Payment',
+          style:TextStyle(
+            color:Colors.black54,
+            fontSize:10.0,
+            fontWeight:FontWeight.bold,
+          ),
+          ) ,
+          ),
+        body: Center(
+          child:Column(children: <Widget>[
+            Padding(
+              padding:EdgeInsets.symmetric(horizontal:16.0,vertical:8.0),
+              child:Text('Available Policies',
+                 style:TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+              ) ,
+              ),
+            SizedBox(height:50.0),
+            GestureDetector(
+              onTap:()=>print("Get smart device discounts"),  // Goes to the smart device discounts page 
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius:BorderRadius.circular(20.0),
+                  color:Colors.lightBlueAccent[100],
+                ),
+                height:60.0,
+                width:350.0,
+                child:Center(
+                  child: Text(
+                    'Get Smart Devices Discounts',
+                   style:TextStyle(
+                     fontSize:20.0,
+                     fontWeight:FontWeight.bold,
+                   ) 
+                  ),
+                )
+                
+              ),
+            ),
+            SizedBox(height:20.0),
+            RadioGroup(),     
+          ],
+
+          ),
+      ),
+       
+    );
+  }
+}
+
+
+class RadioGroup extends StatefulWidget {
+  @override
+  _RadioGroupState createState() => _RadioGroupState();
+}
+class _RadioGroupState extends State<RadioGroup> {
+
+  String default_choice = data['policies'][0];
+  int default_index = 0;
+  List<Mapping>choices = new List<Mapping>();
+
+ 
+  @override
+  void initState(){
+     super.initState();
+     for( int i=0;i<data['policies'].length;i++){
+       choices.add(new Mapping(i,data['policies'][i]));
+  }
+
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Wrap(
+          children: <Widget>[
+          Container(
+            child:Column(
+              children:choices.map((data)=>RadioListTile(
+               // title:Text('${data.user_choice}'),
+               title:Row(
+                 children: <Widget>[
+                   Expanded(
+                     flex:7,
+                     child:Text(
+                       '${data.user_choice}',
+                       overflow:TextOverflow.ellipsis,
+                       maxLines:3,
+                     ),
+                   ),
+                   Expanded(
+                    flex:1,
+                    child:InkWell(
+                      child:Icon(
+                        Icons.attach_money,
+                        color:Colors.blue,
+                        size:20.0,),
+                    ),
+                    ),
+                    Expanded(     //Added a temporary cost to each policy .
+                      flex:2, 
+                      child:Text(
+                        '2000',
+                      ),
+                    ),
+                 ],
+               ),
+                groupValue:default_index,
+                activeColor :Colors.blueAccent,
+                value:data.index,
+                onChanged:(value){ 
+                  // A radio button gets selected only when groupValue is equal to value of the respective radio button 
+                  setState((){
+                    default_choice=data.user_choice ;
+                    default_index=value;  //To make groupValue equal to value for the radio button .
+                  });
+                },
+                )).toList(),
+            ),
+          ),
+        ],
+      ),
+    ],
+    );
+  }
+}
